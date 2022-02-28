3249ac01d837b834036f736633ddf6d32d1d9c2c
Venkatesan Selvaraj <78368102+Vensven123@users.noreply.github.com>
2022-02-28 16:20:40 +0530

    Added controller and html page

---
+++ Historic_DataController.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanDigitization_web.Controllers
+{
+    public class Historic_DataController : Controller
+    {
+        // GET: Historic_Data
+
+        public readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        string Baseurl = @System.Configuration.ConfigurationManager.AppSettings["url"];
+
+        public async Task<ActionResult> Historical_Data()
+        {
+            if (Session["CompanyCode"] != null && Session["PlantCode"] != null && Session["LineCode"] != null)
+            {
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(Baseurl);
+                        client.DefaultRequestHeaders.Clear();
+                        var user1 = Session["Token"].ToString() + ':' + Session["UserName"];
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user1.ToString());
+                        var responseMessage = await client.GetAsync("api/UserSettings/GetEmployee");
+                        if (responseMessage.IsSuccessStatusCode)
+                        {
+                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                            return View();
+                        }
+                        else
+                        {
+                            TempData["message"] = "Token is not valid";
+                            return RedirectToAction("Login", "Main");
+                        }
+
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn(e);
+                    return RedirectToAction("Error_Page", "Main");
+                }
+            }
+            else {
+                return RedirectToAction("Settings_err", "Main");
+            }
+                
+        }
+ 
+    }
+}
